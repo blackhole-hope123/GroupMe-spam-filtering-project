76b943845c47734d85cf6aa18468b68f8801f7f8
blackhole-hope123 <smile-369@hotmail.com>
2025-11-06 14:45:34 -0500

    minor updates

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import requests
 
 res = requests.post("http://127.0.0.1:5000/groupme", json={"text": "Yikes is there a bike rack instead them? I honestly do not want to risk my car getting towed or ticketed"})
-res = requests.post("http://127.0.0.1:5000/groupme", json={"text": "Anyone selling any more Michigan tickets for Saturday? Looking for 2 more, doesn\u2019t matter and where and they don\u2019t have to be in the same section"})
+res = requests.post("http://127.0.0.1:5000/groupme", json={"text": "Selling Michigan tickets for Saturday. DM me if interested."})
